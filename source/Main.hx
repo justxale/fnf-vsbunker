2d1ffff55aa302a9bf9498654e9f0e019618a3b9
Xale <alexev2401@gmail.com>
2022-06-21 13:38:55 +0300

    Initial commit

---
+++ source/Main.hx
@@ -0,0 +1,129 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxState;
+import flixel.tweens.FlxTween;
+import openfl.Assets;
+import openfl.Lib;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import lime.app.Application;
+import lime.ui.WindowAttributes;
+import MusicBeatState;
+
+class Main extends Sprite
+{
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var framerate:Int = 60; // How many frames per second the game should run at.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	public static var appTitle:String = "Friday Night Funkin': Grafex Engine";
+
+	final normalFps:Int = ClientPrefs.framerate;
+	final lowFps:Int = 20;
+	var focusMusicTween:FlxTween;
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+	}
+
+	public function new()
+	{
+		super();
+
+		if (stage != null)
+		{
+			init();
+			
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+Application.current.window.onFocusOut.add(onWindowFocusOut);
+		Application.current.window.onFocusIn.add(onWindowFocusIn);
+	}
+
+
+	function onWindowFocusOut()
+		{
+			trace("Game unfocused");
+	
+			if(!ClientPrefs.autoPause)
+			{
+			// Lower global volume when unfocused
+			if (focusMusicTween != null)
+				focusMusicTween.cancel();
+			focusMusicTween = FlxTween.tween(FlxG.sound, {volume: FlxG.sound.volume * 0.2}, 0.5);
+	
+			// Conserve power by lowering draw framerate when unfocuced
+			FlxG.drawFramerate = 20;
+			}
+		}
+	
+		function onWindowFocusIn()
+		{
+			trace("Game focused");
+	
+			if(!ClientPrefs.autoPause)
+				{
+			// Normal global volume when focused
+			if (focusMusicTween != null)
+				focusMusicTween.cancel();
+			focusMusicTween = FlxTween.tween(FlxG.sound, {volume: FlxG.sound.volume * 5}, 0.5);
+	
+			// Bring framerate back when focused
+			FlxG.drawFramerate = ClientPrefs.framerate;
+			FlxG.updateFramerate = ClientPrefs.framerate;
+				}
+		}
+	
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
+
+		#if !debug
+		initialState = TitleState;
+		#end
+
+		ClientPrefs.loadDefaultKeys();
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+
+		#if !mobile
+                addChild(new FPSMem(10, 3, 0xFFFFFF));
+		#end
+
+
+		#if html5
+		//FlxG.autoPause = false;
+		FlxG.mouse.visible = false;
+		#end
+	}
+}
